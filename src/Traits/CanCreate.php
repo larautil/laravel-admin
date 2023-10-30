164995ee8f754a0bab6933e90910351fab7469c4
LaraUtil <149219439+larautil@users.noreply.github.com>
2023-10-31 01:04:59 +0800

    Adm 3 refactor to resource controller (#3)
    * add Button class;
    add button blade view
    update Widget markdown doc
    
    * refactor(controller):
    - add abstract class ResourceController
    - add trait CanCreate
    - add trait CanDelete
    - add trait CanEdit
    - add trait HasGrid
    - add trait HasShow
    
    ---------
    
    Co-authored-by: Laravel Utility <larautil@outloook.com>

---
+++ src/Traits/CanCreate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Encore\Admin\Traits;
+
+use Encore\Admin\Layout\Content;
+
+trait CanCreate
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return mixed
+     */
+    public function store()
+    {
+        return $this->form()->store();
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     *
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->title($this->title())
+            ->description($this->description['create'] ?? trans('admin.create'))
+            ->body($this->form());
+    }
+}
